f7f3ac94aa4f476e9438f7adeb978905bacd7e48
jose <joseflores8082@gmail.com>
2015-01-21 13:20:13 -0800

    changes to diles

---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+// Sticky Header
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+        $('.main_h').addClass('sticky');
+    } else {
+        $('.main_h').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.main_h').removeClass('open-nav');
+    } else {
+        $('.main_h').addClass('open-nav');
+    }
+});
+
+$('.main_h li a').click(function() {
+    if ($('.main_h').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_h').removeClass('open-nav');
+    }
+});
+
+// navigation scroll lijepo radi materem
+$('nav a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
+});
+
+var contentPadding = $(window).height();
+
+$('.content').css('padding-top', contentPadding);
+
+$(window).resize(function(){
+  var contentPadding = $(window).height();
+   $('.content').css('padding-top', contentPadding);
+});
+
+
+// work section responsive
+
+var percentage = $(window).width();
+var newWidth = percentage/3;
+
+$('.work').css('width', newWidth);
+// owl carousel 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:1,
+            nav:false
+        },
+        1000:{
+            items:1,
+            nav:true,
+            loop:false
+        }
+    }
+});
+
+
+});
